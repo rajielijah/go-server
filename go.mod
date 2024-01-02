52f22821f97f9e4412091ab0b94be64b74a35110
jahbie <eraji@crmonline.com.au>
2024-01-02 13:13:59 +0100

    resturcting to mvc patter

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rajielijah/go-server
+
+go 1.19
--- main1.go
+++ main1.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Request Recieed")
-		w.Write([]byte("We are here"))
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			data := view.Response{
+				Code: http.StatusOK,
+				Body: "pong",
+			}
+			json.NewEncoder(w).Encode(data)
+
+			fmt.Println("Request Recieed")
+			w.Write([]byte("We are here"))
+		}
 	})
 	http.ListenAndServe("localhost:3000", mux)
 }
